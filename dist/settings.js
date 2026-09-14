b462ef509c20985e55ab07ffed938282920d556e
dosordie <14365099+dosordie@users.noreply.github.com>
2026-09-14 23:04:11 +0200

    build: sync compiled settings output

--- dist/settings.js
+++ dist/settings.js
@@ -1,3 +1,4 @@
 export const PLATFORM_NAME = 'MatterPower';
 export const PLUGIN_NAME = 'homebridge-matter-power';
 export const MIN_HOMEBRIDGE_VERSION = '2.4.0';
+//# sourceMappingURL=settings.js.map
