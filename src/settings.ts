ed1cdec2b0439711aff5618f1a4218bcca57052f
dosordie <14365099+dosordie@users.noreply.github.com>
2026-09-14 21:55:59 +0200

    chore: require Homebridge 2.4

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,3 @@
 export const PLATFORM_NAME = 'MatterPower';
 export const PLUGIN_NAME = 'homebridge-matter-power';
-export const MIN_HOMEBRIDGE_VERSION = '2.3.0';
+export const MIN_HOMEBRIDGE_VERSION = '2.4.0';
